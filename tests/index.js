30e5565a759c6dba084a039dc668a3b3a769229b
Alejandro Fernández Haro <afharo@gmail.com>
2015-08-13 13:07:10 +0200

    Use of 'strict' javascript

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,15 @@
-var USBSerial = require('../index.js');
+/*jslint node: true */
+'use strict';
+
+var USBSerial, debug, usb;
+
+USBSerial = require('../index.js');
 if (process.env.DEBUG) {
-	var debug = require('debug')('modem-serial:test');
+	debug = require('debug')('modem-serial:test');
 } else {
-	var debug = function(text) {console.log(text)};
+	debug = function() {console.log.apply(this, arguments);};
 }
-var usb = USBSerial('/dev/cu.HUAWEIMobile-Pcui');
+usb = new USBSerial('/dev/cu.HUAWEIMobile-Pcui');
 
 usb.getNotified(function (field, oldValue, newValue) {
   debug('%s changed from %s to %s', field, oldValue, newValue);
@@ -14,11 +19,11 @@ usb.getNotified(function (field, oldValue, newValue) {
 usb.onCallStatusChange(function (status, ts) {
 	debug('[%d] Call is in status: %s', ts, status);
 	debug(usb.getCallInfo());
-})
+});
 
 if (process.env.NUMBER) {
 	setTimeout(function(){
-		debug("CALLING")
+		debug("CALLING");
 		usb.call(process.env.NUMBER, 10);
 	},10000);
 }
